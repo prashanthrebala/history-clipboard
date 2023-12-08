b8f5ef3713731d552b637a9b9ee7f3fa414fac3e
Prashanth Rebala <prashanth.rebala@gmail.com>
2023-12-08 17:51:07 -0500

    copy-to-clipboard from gui

--- clipboard_gui.py
+++ clipboard_gui.py
@@ -1,29 +1,29 @@
 import tkinter as tk
+import pyperclip
 
 class ClipboardGUI:
     def __init__(self, elements):
         self.elements = elements
         self.root = tk.Tk()
         self.root.title("Clipboard History")
-
-        # Make the GUI unresizable
         self.root.resizable(width=False, height=False)
 
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         window_width = 200
         window_height = 33 * len(self.elements)
-        window_x = screen_width - window_width  # Adjust 10 for a small margin
-        window_y = screen_height - window_height  # Adjust 10 for a small margin
+        window_x = screen_width - window_width
+        window_y = screen_height - window_height
 
         self.root.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
-
         self.create_labels()
+
 
     def label_click(self, event):
         clicked_label = event.widget
-        print(f'You clicked on: {clicked_label["text"]}')
-        self.root.destroy()
+        pyperclip.copy(clicked_label["text"])
+        self.close()
+
 
     def create_labels(self):
         for element in self.elements:
@@ -31,5 +31,10 @@ class ClipboardGUI:
             label.pack(fill=tk.X)  # Make the label expand horizontally
             label.bind('<Button-1>', self.label_click)
 
+
     def run(self):
         self.root.mainloop()
+
+
+    def close(self):
+        self.root.destroy()
